585534a1dc32087bd8cf74f24667b1872120ce37
rifan-t <rifanzprr@gmail.com>
2023-11-09 15:20:37 +0700

    Make Dashboard

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -1,10 +1,11 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class dashboard extends CI_Controller {
+class Dashboard extends CI_Controller {
 
 	public function index()
 	{
 		$this->load->view('V_rifan');
 	}
 }
+    
