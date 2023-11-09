292ea24da12df0fc21b42991576c8194c3274ea4
rifan-t <rifanzprr@gmail.com>
2023-11-09 11:19:00 +0700

    C_Rifan

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -1,10 +1,10 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class dashboard extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('V_rifan');
 	}
 }
